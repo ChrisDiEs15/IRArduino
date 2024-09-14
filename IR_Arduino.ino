98a2f7cb9b921a4b3729c1e62f119ffda00062ab
Christian Alejandro Diaz Espinosa <95449359+ChrisDiEs15@users.noreply.github.com>
2024-09-13 23:14:39 -0600

    Update IR_Arduino.ino

--- IR_Arduino.ino
+++ IR_Arduino.ino
@@ -1,6 +1,6 @@
 #include <IRremote.h>
 
-#define IR_SEND_PIN 3
+#define IR_SEND_PIN 2
 #define IR_RECEIVE_PIN 11     // Se puede omitir la definición de IR_RECEIVE_PIN si se utiliza el pin 2, que es el predeterminado para la recepción en la librería IRremote.
 unsigned long key_value = 0;  // Variable para almacenar el valor de la tecla
 
@@ -23,30 +23,9 @@ void loop() {
     Serial.print("Código recibido en HEX: ");
     Serial.println(key_value, HEX);  //se imprime en valor hexadecimal la informacion proveniente del control remoto, se recive como argumento key_value
 
-    // Imprime el protocolo detectado
-    switch (IrReceiver.decodedIRData.protocol) {
-      case SAMSUNG:
-        Serial.println("Protocolo Samsung detectado");
-        break;
-      case LG:
-        Serial.println("Protocolo LG detectado");
-        break;
-      case NEC:
-        Serial.println("Protocolo NEC detectado");
-        break;
-      case SONY:
-        Serial.println("Protocolo Sony detectado");
-        break;
-      case RC5:
-        Serial.println("Protocolo RC5 detectado");
-        break;
-      case RC6:
-        Serial.println("Protocolo RC6 detectado");
-        break;
-      default:
-        Serial.println("Protocolo desconocido");
-        break;
-    }
+    IrReceiver.printIRResultShort(&Serial); //Imprime la informacion recibida, protocolo, comando recibido, informacion RAW, bits
+    IrReceiver.printIRSendUsage(&Serial);//Imprime la manera correcta de enviarlo a traves de IRSender 
+
     Serial.println();
     IrReceiver.resume();
     Serial.println("Espere al siguiente mensaje para recibir el siguiete codigo");
@@ -61,93 +40,95 @@ void loop() {
   }
 }
 //funcion que imprime los creditos
-void imprimirCreditos() {}
+void imprimirCreditos() {
+  
+}
 
 void menu(char opc) {
   switch (opc) {
     case 'a':
       Serial.println("Opción a: Encender Blu-ray");
-      Serial.flush();                    // Espera a que se envíen completamente los datos por el puerto serial
-      IrSender.sendNEC(0xCF302D2D, 32);  // Código IR para encender el Blu-ray
+      Serial.flush();  // Espera a que se envíen completamente los datos por el puerto serial
+      IrSender.sendSony(0x1C5A, 0x15, 2, 20);
       Serial.println("Enviado");
       delay(2000);
       break;
     case 'b':
       Serial.println("Opción b: Abrir bandeja");
       Serial.flush();                    // Espera a que se envíen completamente los datos por el puerto serial
-      IrSender.sendNEC(0xC9362D2D, 32);  // Código IR para abrir la bandeja
+      IrSender.sendSony(0x1C5A, 0x16, 2, 20);// Código IR para abrir la bandeja
       Serial.println("Enviado");
-      delay(2000);                       // Espera a 2 segundos antes de continuar
+      delay(2000);  // Espera a 2 segundos antes de continuar
       break;
     case 'c':
       Serial.println("Opción c: Avanzar");
       Serial.flush();                    // Espera a que se envíen completamente los datos por el puerto serial
-      IrSender.sendNEC(0xCC332D2D, 32);  // // Código IR para la acción "Avanzar"
+      IrSender.sendSony(0x1C5A, 0x1C, 2, 20); // // Código IR para la acción "Avanzar"
       Serial.println("Enviado");
-      delay(2000);                       // Espera a 2 segundos antes de continuar
+      delay(2000);  // Espera a 2 segundos antes de continuar
       break;
     case 'd':
       Serial.println("Opción d: Retroceder");
       Serial.flush();                   // Espera a que se envíen completamente los datos por el puerto serial
-      IrSender.sendNEC(0xCD32D2D, 32);  // Código IR para la accion "Retroceder" pararetroceder
+      IrSender.sendSony(0x1C5A, 0x1B, 2, 20);  // Código IR para la accion "Retroceder" pararetroceder
       Serial.println("Enviado");
-      delay(2000);                      // Espera a 2 segundos antes de continuar
+      delay(2000);  // Espera a 2 segundos antes de continuar
       break;
     case 'e':
       Serial.println("Opción e : Siguiente");
       Serial.flush();                    // Espera a que se envíen completamente los datos por el puerto serial
-      IrSender.sendNEC(0xCD342D2D, 32);  // Código IR para la accion "Siguiente" parasiguiente
+      IrSender.sendSony(0x1C5A, 0x56, 2, 20);  // Código IR para la accion "Siguiente" parasiguiente
       Serial.println("Enviado");
-      delay(2000);                       // Espera a 2 segundos antes de continuar
+      delay(2000);  // Espera a 2 segundos antes de continuar
       break;
     case 'f':
       Serial.println("Opción f: Regresar");
       Serial.flush();                    // Espera a que se envíen completamente los datos por el puerto serial
-      IrSender.sendNEC(0xCA352D2D, 32);  // Código IR para la accion "Regresar" pararegresar
+      IrSender.sendSony(0x1C5A, 0x57, 2, 20);  // Código IR para la accion "Regresar" pararegresar
       Serial.println("Enviado");
-      delay(2000);                       // Espera a 2 segundos antes de continuar
+      delay(2000);  // Espera a 2 segundos antes de continuar
       break;
     case 'g':
       Serial.println("Opción g: Reproduccion");
       Serial.flush();                    // Espera a que se envíen completamente los datos por el puerto serial
-      IrSender.sendNEC(0xCE312D2D, 32);  // Código IR para la accion "Reproducir" parareproducir
+      IrSender.sendSony(0x1C5A, 0x1A, 2, 20);  // Código IR para la accion "Reproducir" parareproducir
       Serial.println("Enviado");
-      delay(2000);                       // Espera a 2 segundos antes de continuar
+      delay(2000);  // Espera a 2 segundos antes de continuar
       break;
     case 'h':
       Serial.println("Opción h: Pausa");
       Serial.flush();                    // Espera a que se envíen completamente los datos por el puerto serial
-      IrSender.sendNEC(0xC7382D2D, 32);  // Código IR para la accion "Pausar" parapausar
+      IrSender.sendSony(0x1C5A, 0x19, 2, 20);  // Código IR para la accion "Pausar" parapausar
       Serial.println("Enviado");
-      delay(2000);                       // Espera a 2 segundos antes de continuar
+      delay(2000);  // Espera a 2 segundos antes de continuar
       break;
     case 'i':
       Serial.println("Opción i: Arriba");
       Serial.flush();                     // Espera a que se envíen completamente los datos por el puerto serial
-      IrSender.sendNEC(0xCB8472D2D, 32);  // Código IR para la accion "Arriba" paraarriba
+      IrSender.sendSony(0x1C5A, 0x39, 2, 20);  // Código IR para la accion "Arriba" paraarriba
       Serial.println("Enviado");
-      delay(2000);                        // Espera a 2 segundos antes de continuar
+      delay(2000);  // Espera a 2 segundos antes de continuar
       break;
     case 'j':
       Serial.println("Opción j: Abajo");
       Serial.flush();                    // Espera a que se envíen completamente los datos por el puerto serial
-      IrSender.sendNEC(0xB7482D2D, 32);  // Código IR para la accion "Abajo" paraabajo
+      IrSender.sendSony(0x1C5A, 0x3A, 2, 20);  // Código IR para la accion "Abajo" paraabajo
       Serial.println("Enviado");
-      delay(2000);                       // Espera a 2 segundos antes de continuar
+      delay(2000);  // Espera a 2 segundos antes de continuar
       break;
     case 'k':
       Serial.println("Opción k: izquierda");
       Serial.flush();                    // Espera a que se envíen completamente los datos por el puerto serial
-      IrSender.sendNEC(0xA6592D2D, 32);  // Código IR para la accion "Izquierda" paraizquierda
+      IrSender.sendSony(0x1C5A, 0x3B, 2, 20);  // Código IR para la accion "Izquierda" paraizquierda
       Serial.println("Enviado");
-      delay(2000);                       // Espera a 2 segundos antes de continuar
+      delay(2000);  // Espera a 2 segundos antes de continuar
       break;
     case 'l':
       Serial.println("Opción l: Derecha");
       Serial.flush();                    // Espera a que se envíen completamente los datos por el puerto serial
-      IrSender.sendNEC(0xA55A2D2D, 32);  // Código IR para la accion "Derecha" para derecha
+      IrSender.sendSony(0x1C5A, 0x3C, 2, 20);  // Código IR para la accion "Derecha" para derecha
       Serial.println("Enviado");
-      delay(2000);                       // Espera a 2 segundos antes de continuar
+      delay(2000);  // Espera a 2 segundos antes de continuar
       break;
     case 'm':
       Serial.println("Imprimir creditos");
